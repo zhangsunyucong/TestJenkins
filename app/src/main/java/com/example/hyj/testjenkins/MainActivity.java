64ac912949c35835eaafd569bcbd8a86ec422e0b
unknown <hyj@htrfid.com>
2017-11-29 17:11:06 +0800

    ss

--- app/src/main/java/com/example/hyj/testjenkins/MainActivity.java
+++ app/src/main/java/com/example/hyj/testjenkins/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 	
-	private String userName = "zhangsunyucong";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
